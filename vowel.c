905b0149a2d9715302912f291027fa1ab3f7feec
Rachannaa <32996438+Rachannaa@users.noreply.github.com>
2017-10-25 22:03:27 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -5,10 +5,10 @@ char a;
 scanf("%c",&a);
 if(a=='a' || a=='A' || a=='e' || a=='E' || a=='i' || a=='I' || a=='o' || a=='O' || a=='u' || a=='U')
 {
-printf("%c is a vowel",a);
+printf("\n%c is a vowel",a);
 }
 else
 {
-printf("%c is not a vowel",a);
+printf("\n%c is not a vowel",a);
 }
 }
